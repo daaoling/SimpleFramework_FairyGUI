4dd72c1226b4621ab77c9d0b4a7bbd2218821991
daaoling <1294246096@qq.com>
2017-01-04 20:32:58 +0800

    初步跑通UI框架测试

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-#XXJN_Client
+#tolua fairygui 一个结合示例框架
+
+## 2016-1-4 
+mvc 框架搭建
+初步完成场景管理
--- push.sh
+++ push.sh
@@ -2,3 +2,4 @@
 git add --all
 git commit -m "初步跑通UI框架测试" 
 git push origin master
+git push github master
